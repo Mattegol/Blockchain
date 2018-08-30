f9d06a3ba2a3f84dd4eaf301c5f4d33dc76d3419
Mats Egnell <mattegol.egnell@gmail.com>
2018-08-30 18:52:15 +0200

    Add connect to blockchain peers

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,8 +3,6 @@ const Websocket = require('ws');
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
-// $ HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
-
 class P2pServer {
     constructor(blockchain) {
         this.blockchain = blockchain;
@@ -12,15 +10,27 @@ class P2pServer {
     }
 
     listen() {
-        const server = new Websocket({ port: P2P_PORT });
+        const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening to peer-to-peer connections on: ${P2P_PORT}`)
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
-
     }
 }
+
+module.exports = P2pServer;
 
